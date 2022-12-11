a73d132208667e1f4843dea2963e43e18d18a5ae
Nathanael-Adi <6181901041@student.unpar.ac.id>
2022-12-11 17:55:27 +0700

    update chart.js

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "got_character_book1";
+$connect = mysqli_connect($host, $user, $pass, $db);
+if (mysqli_connect_error())
+{
+    echo "Koneksi gagal"
+}
+?>
