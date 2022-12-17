e0e2353611080d99f735589128935c7449737005
Nathanael-Adi <6181901041@student.unpar.ac.id>
2022-12-17 18:00:03 +0700

    update

---
+++ buku4.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css"/> 
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css"> 
+    <link rel="stylesheet" href="buku1.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script> 
+    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script> 
+    <title>Buku 4 Game of Thrones</title>
+</head>
+<body>
+    <div class="topnav">
+        <nav>
+            <h2 class="logo"><a href="index.php" style="text-decoration:none; color:white">Mother Dragon</a></h2>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="book.php">Buku</a></li>
+                <li><a href="graf.php">Graf</a></li>
+                <li><a href="grafik.php">Grafik</a></li>    
+            </ul>
+        </nav>
+    </div>
+
+    <main class="bb" style="height:200px;">
+            <div class="home__container">
+                <div class="home__data">
+                    <h3 class="home__title" style="color:#0e0f11; justify-contents:center">Buku Keempat Game of Thrones</h3>                  
+                </div>
+            </div>
+
+    </main>
+
+    <table id="example" class="table table-striped table-bordered" style="width:100%; color:black">
+    <thead>
+            <tr>
+                <th>Source</th>
+                <th>Target</th>
+                <th>Type</th>
+                <th>Weight</th>
+                <th>Book</th>
+            </tr>
+    </thead>
+
+    <tbody>
+            <?php
+                include 'connect.php';
+                $buku4 = mysqli_query($koneksi, "select * from book4");
+                while($row = mysqli_fetch_array($buku4)){
+                    echo "<tr>
+                    <td>".$row['Source']."</td>
+                    <td>".$row['Target']."</td>
+                    <td>".$row['Type']."</td>
+                    <td>".$row['weight']."</td>
+                    <td>".$row['book']."</td> 
+                    </tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+    <script>
+    $(document).ready(function () {
+    $('#example').DataTable();
+    });
+</script>
+</body>
+
+
+
+
+
+</html>
--- connect.php
+++ connect.php
@@ -1,14 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root";
+$username = "root";
 $pass = "";
-$db = "got_character_book1";
-$connect = mysqli_connect($host, $user, $pass, $db);
-if (mysqli_connect_error())
-{
-    echo "Koneksi gagal"
-}
-else{
-    echo "Koneksi berhasil"
-}
+$database = "manpro";
+$koneksi = mysqli_connect($host, $username, $pass, $database);
 ?>
