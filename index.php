f9d2db4d28158134d3d07c81951d9c73c9665cf6
carizzaaretha <6181801048@student.unpar.ac.id>
2022-12-17 17:25:14 +0700

    modifikasi file dan penambahan file

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <!DOCTYPE html>
 <html lang="en">
@@ -57,65 +56,4 @@
         </div>
     </section>
 </body>
-=======
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
-    <title>TUBES MANPRO</title>
-</head>
-<body>
-    <div class="topnav">
-        <nav>
-            <h2 class="logo">Mother Dragon</h2>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="buku.php">Buku</a></li>
-                <li><a href="">Graf</a></li>
-                <li><a href="grafik.php">Grafik</a></li>    
-            </ul>
-
-        </nav>
-    </div>
-
-    <main class="l-main">
-        <section class="home" id="home">
-            <div class="home__container bd-grid">
-                <div class="home__img">
-                    
-                </div>
-
-                <div class="home__data">
-                    <h1 class="home__title">Game of Thrones</h1>
-                    <p class="home__description">Website pencarian graf dan grafik interaksi antar karakter Game of Thrones</p>
-                </div>
-            </div>
-        </section>
-    </main>
-
-    <section class="about">
-        <div class="main">
-            <img src="about.jpg" alt="about">
-            <div class="about_text">
-                <h2>About this project</h2>
-                <h5>Website untuk Graf & Grafik</h5>
-                <p>Website ini dibangun untuk menunjukan graf yang merepresentasikan frekuensi interaksi antar karakter
-                    game of thrones. Graf yang dibuat merupakan graf tidak berarah. Website ini juga memiliki fitur untuk
-                    menunjukan grafik batang untuk mencari TOP-10 karakter yang paling banyak interaksi dengan karakter lain.
-                </p>
-                <button>Cek Graf</button>
-            </div>
-        </div>
-
-    </section>
-
-</body>
-
-
->>>>>>> 2b3b2b3f84833ce1a4a11fb735aebb65187b09cf
 </html>
